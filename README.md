db80afe7c2df8613262fb184aa8ced75924289bc
Rufus Pollock <rufus.pollock@okfn.org>
2012-11-24 16:56:27 +0000

    [README,scripts/extract][m]: detailed steps in README plus extraction script now distinguishes files with same doc id by tab type.

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 TODO: some more info
 
-## Getting the data out of the mongo db
+## Data Processing Pipeline
 
-`mongoexport --host opented.org --db opented --username iacc --password gohack --collection dumps --csv --fields "zhtml,doc_id,timestamp" | head -n 5000 > tmp.csv`
+Structured data is in a MongoDB at opented.org/opented
+
+Unstructured cached HTML pages are also in the that DB in a collection called dumps (in future this data should probably go direct to s3!).
+
+### 1. Get dumps onto s3
+
+Get data out of mongodb:
+
+    mongoexport --host opented.org --db opented --username iacc --password gohack --collection dumps --csv --fields "zhtml,doc_id,timestamp" | head -n 5000 > tmp.csv
+
+Then use extract.py:
+
+    python scripts/extract.py
+
+This will produce a whole bunch of files in `cache/dumps`
+
+Now push these to s3:
+
+    s3cmd sync --acl-public cache/dumps/ s3://files.opented.org/scraped/
+
+You will find the index of files at: <http://files.opented.org.s3.amazonaws.com/scraped/index.json>
+
+### 2. Scraping content
+
+Now it's time to scrape some content!
+
+We've written a nodejs scraper:
+
+    node scripts/scraper.js
+
+Data will be written to cache/dumps/{docid}/extracted.json
+
--- scripts/extract.py
+++ scripts/extract.py
@@ -3,20 +3,38 @@ import csv
 import zlib
 import binascii
 import os
+import re
+import json
+import collections
 
 fp = 'cache/dumps.csv'
 outdir = 'cache/dumps'
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
+index = collections.defaultdict(list)
+
 for count,row in enumerate(csv.DictReader(open(fp))):
     data = binascii.unhexlify(row['zhtml'])
+    docid = row['doc_id']
+    docdir = os.path.join(outdir, docid)
     try: 
         uncompressed = zlib.decompress(data)
-        outpath = os.path.join(outdir, row['doc_id'] + '.html')
+        # page title
+        # out = re.findall('<title>(.*)</title>', uncompressed)
+        out = re.findall('<li class="activated"><div>\s+<a[^>]*>([^<]*)</a>', uncompressed,
+                re.MULTILINE)
+        pagetab = out[0].strip().lower().replace(' ', '')
+        index[docid].append(pagetab)
+        if not os.path.exists(docdir):
+            os.makedirs(docdir)
+        outpath = os.path.join(docdir, pagetab + '.html')
         open(outpath, 'w').write(uncompressed)
-    except:
+    except zlib.error:
         print 'had error with ' + row['doc_id']
     if count > 500:
         break
     
+indexpath = os.path.join(outdir, 'index.json')
+json.dump(index, open(indexpath, 'w'), indent=2, sort_keys=True)
+
