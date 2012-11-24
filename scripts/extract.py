cc653ce8d851216919521a89bd6830fed7008d81
Callum Locke <callum.locke@gmail.com>
2012-11-24 18:06:36 +0000

    only save summary file

--- scripts/extract.py
+++ scripts/extract.py
@@ -14,27 +14,27 @@ if not os.path.exists(outdir):
 
 index = collections.defaultdict(list)
 
-for count,row in enumerate(csv.DictReader(open(fp))):
+for count, row in enumerate(csv.DictReader(open(fp))):
     data = binascii.unhexlify(row['zhtml'])
     docid = row['doc_id']
     docdir = os.path.join(outdir, docid)
-    try: 
+    try:
         uncompressed = zlib.decompress(data)
         # page title
         # out = re.findall('<title>(.*)</title>', uncompressed)
         out = re.findall('<li class="activated"><div>\s+<a[^>]*>([^<]*)</a>', uncompressed,
                 re.MULTILINE)
         pagetab = out[0].strip().lower().replace(' ', '')
-        index[docid].append(pagetab)
-        if not os.path.exists(docdir):
-            os.makedirs(docdir)
-        outpath = os.path.join(docdir, pagetab + '.html')
-        open(outpath, 'w').write(uncompressed)
+        if pagetab == 'summary':
+            index[docid].append(pagetab)
+            if not os.path.exists(docdir):
+                os.makedirs(docdir)
+            outpath = os.path.join(docdir, pagetab + '.html')
+            open(outpath, 'w').write(uncompressed)
     except zlib.error:
         print 'had error with ' + row['doc_id']
-    if count > 500:
+    if count > 100000:
         break
-    
+
 indexpath = os.path.join(outdir, 'index.json')
 json.dump(index, open(indexpath, 'w'), indent=2, sort_keys=True)
-
